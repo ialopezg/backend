ecab691ff02f994f32f0bb04ea1c38d09ddf33da
Isidro A. López G <me@ialopezg.com>
2022-07-22 16:30:33 -0400

    style: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
-    'standard'
+    'standard',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
   rules: {
-  }
-}
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-useless-constructor': 'off',
+    'space-before-function-paren': 'off',
+    semi: ['error', 'always'],
+  },
+};
