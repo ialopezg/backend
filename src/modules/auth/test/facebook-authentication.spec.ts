e7cfec3ea22eb2cbec12ca0f4e999c20d62024de
Isidro A. López G <me@ialopezg.com>
2022-07-23 09:31:39 -0500

    test: improve test

--- src/modules/auth/test/facebook-authentication.spec.ts
+++ src/modules/auth/test/facebook-authentication.spec.ts
@@ -5,21 +5,15 @@ import { FacebookApi } from '../interfaces';
 import { FacebookAuthService } from '../services/facebook-auth.service';
 
 describe('FacebookAuthService', () => {
-  type ServiceTypes = {
-    auth: FacebookAuthService,
-    api: MockProxy<FacebookApi>,
-  }
+  let api: MockProxy<FacebookApi>;
+  let auth: FacebookAuthService;
 
-  const createServices = (): ServiceTypes => {
-    const api = mock<FacebookApi>();
-    const auth = new FacebookAuthService(api);
-
-    return { api, auth };
-  };
+  beforeEach(() => {
+    api = mock();
+    auth = new FacebookAuthService(api);
+  });
 
   it('should call FacebookAuthenticationService with correct params', async () => {
-    const { api, auth } = createServices();
-
     await auth.perform({ token: 'token' });
 
     expect(api.loadUser).toHaveBeenCalledWith({ token: 'token' });
@@ -27,7 +21,6 @@ describe('FacebookAuthService', () => {
   });
 
   it('should return AuthenticationError when FacebookApi returns undefined', async () => {
-    const { api, auth } = createServices();
     api.loadUser.mockResolvedValueOnce(undefined);
     const authResult = await auth.perform({ token: 'token' });
 
